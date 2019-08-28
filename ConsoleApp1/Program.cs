117f0fbccad610a92f781c930cf2bec00ce98060
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-08-28 15:05:17 +0300

    1_Operands
    string message = (numb01 > numb02) ? "Numb01 > Numb02" : "Numb02 > Numb01";

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First number: ");
+            int? sayi01 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            int? sayi02 = Convert.ToInt32(Console.ReadLine());
+
+            string message = (sayi01>sayi02) ? "First number > Second number" : "Second number > First number";
+
+            Console.WriteLine(message + "\nPush enter for continue..");
+            Console.ReadKey();
+
+
+            Console.WriteLine("Give me a number: ");
+            int? sayi03 = int.Parse(Console.ReadLine());
+
+            string evenOrNot = (sayi03 % 2 != 0) ? "The number is odd" : "The number is even";
+
+            Console.WriteLine(evenOrNot + "\nHit enter buttonn for exit.");
+            Console.ReadLine();
+        }
+    }
+}
