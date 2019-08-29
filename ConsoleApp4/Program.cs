da1e52acca101076be77fd671f1b7c877322621e
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-08-29 11:52:46 +0300

    04_Loops

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting value: ");
+            int numb01 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ending value: ");
+            int numb02 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Increase value: ");
+            int numb03 = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            for (int i = numb01; i <= numb02 ; i+=numb03)
+            {
+                result += i;
+
+                if (i + numb03 <= numb02)
+                {
+                    Console.Write(i + " + ");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+            }
+
+            Console.WriteLine(" = " + result + " NERD");
+
+            Console.ReadLine();
+        }
+    }
+}
