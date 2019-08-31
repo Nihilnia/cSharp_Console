1c8f557686d7991db87ac93096f8eb7e29ec4e3b
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-09-01 00:35:26 +0300

    05_DoWhile

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //char secim = 'y';
+
+            //while (secim == 'y')
+            //{
+            //    Console.WriteLine("First number: ");
+            //    int numb01 = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("Second number: ");
+            //    int numb02 = int.Parse(Console.ReadLine());
+
+            //    Console.WriteLine(Convert.ToInt32(numb01 + numb02) + "Devam etmek istiyor musunuz?: ");
+
+            //    secim = Convert.ToChar(Console.ReadLine());
+            //}
+
+            char secim;
+
+            do
+            {
+                Console.WriteLine("First number: ");
+                int numb01 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Second number: ");
+                int numb02 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Convert.ToInt32(numb01 + numb02) + "Devam etmek istiyor musunuz?: ");
+
+                secim = Convert.ToChar(Console.ReadLine());
+                
+            } while (secim != 'n');
+
+        }
+    }
+}
